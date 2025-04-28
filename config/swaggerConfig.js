ae06f4e428de1e6478cb70fa23ac696c95489776
Adan Rosales <adan.rosales@apptegy.com>
2025-04-28 16:59:55 -0600

    miving swagger to docs

--- config/swaggerConfig.js
+++ config/swaggerConfig.js
@@ -1,7 +1,7 @@
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../swagger.json");
 const setupSwagger = (app) => {
-    app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
 
 module.exports = setupSwagger;
