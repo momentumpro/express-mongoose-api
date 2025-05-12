4ca76a330a764068c13cf0a48838802595405485
Adan Rosales <adan.rosales@apptegy.com>
2025-05-12 12:54:32 -0600

    test cors

--- index.js
+++ index.js
@@ -9,7 +9,24 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true, methods: ['GET', 'POST', 'PUT', 'DELETE'] }));
+
+app.use(
+    cors({
+        origin: ["https://localhost:7500", "https://fe-example.onrender.com/"],
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
+
+let corsOptions = function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers',
+        'Content-Type, Authorization, Content-Length, X-Requested-With');
+    next();
+}
+
+app.use(corsOptions);
 
 connectDB();
 
